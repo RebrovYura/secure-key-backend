8429b34e99887d15f780cf90632398933bfbbcf6
Yuri Rebrov <yurirebrov01@gmail.com>
2023-12-05 19:19:16 +0300

    initial commit

---
+++ src/main/java/com/securekey/securekeybackend/SecureKeyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.securekey.securekeybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureKeyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureKeyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securekey/securekeybackend/SecureKeyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securekey.securekeybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureKeyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
